b61ed297de255c8460999ca73d794ed3ed7be04a
Colin <cln.gardiner@outlook.com>
2021-11-05 12:17:32 +0000

    wrote test for addition of numbers

---
+++ cypress/integration/Operations.spec.js
@@ -0,0 +1,18 @@
+const cypress = require("cypress");
+
+describe.only('check calculator operations', () => {
+  
+  it('user can add two numbers', () => {
+    cy.visit('/')
+    cy.get('p[id="number-display"]').should('not.contain.text')
+    cy.get('button[id="number-button-1"]').click()
+    cy.get('button[id="number-button-2"]').click()
+    cy.get('button[id="operator-add"]').click()
+    cy.get('button[id="number-button-9"]').click()
+    cy.get('p[id="number-display"]').should('have.text','9')
+    cy.get('button[id="number-button-7"]').click()
+    cy.get('button[id="operator-equal"]').click()
+    cy.get('p[id="number-display"]').should('have.text','109')
+  })
+
+});
